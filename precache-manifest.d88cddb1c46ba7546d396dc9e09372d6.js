bbfd6c1ce26ae6cba16361768f406f6eb8448018
Ganesh <ganesh152595@gmail.com>
2020-04-15 16:31:39 +0530

    Updates

---
+++ precache-manifest.d88cddb1c46ba7546d396dc9e09372d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06a3f909152eb861a5ee1bd1ee438630",
+    "url": "/TechQuiz/index.html"
+  },
+  {
+    "revision": "1ddf54d170bfa9d5a3f2",
+    "url": "/TechQuiz/static/css/main.c63d9cba.chunk.css"
+  },
+  {
+    "revision": "ae0337a4f9221cbd81f9",
+    "url": "/TechQuiz/static/js/2.a3bc8817.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TechQuiz/static/js/2.a3bc8817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ddf54d170bfa9d5a3f2",
+    "url": "/TechQuiz/static/js/main.40f35078.chunk.js"
+  },
+  {
+    "revision": "f1b06b5cb6e4dd5aa9f7",
+    "url": "/TechQuiz/static/js/runtime-main.227a82de.js"
+  }
+]);
